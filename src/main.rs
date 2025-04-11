63eb16f629e33459b1b33b1eeb2c87db99a7f631
Matheus Nascimento <matheus.nascimento@byne.com.br>
2025-04-11 13:39:32 -0300

    add decode for list

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,52 @@
 use std::env;
 
-fn decode_string(char_len: i32, text_part: &str) -> (String, String) {
-    let char_len = char_len as usize;
-    let decoded_message = &text_part[0..char_len];
-    let quotation_marks: &str = "\"";
-    return (format!("{}{}{}", quotation_marks, decoded_message, quotation_marks), "".to_string());
-}
-
-fn decode_integer(message: &str) -> (String, String) {
-    let length: usize = message.chars().count() as usize;
-    let decoded_message = &message[1..=length-2];
-    return (decoded_message.to_string(), "".to_string());
-}
-fn decode_list(message: &str) -> (String, String) {
-    let length: usize = message.chars().count() as usize;
-    let mut _inner_message: &str = &message[1..=length-2];
-    let decoded_message: String = "".to_string();
-    return (format!("{}{}{}", "[", decoded_message, "]"), "".to_string());
-}
-
-fn decode(message: &str) -> String {
+fn decode(mut message: String) -> String {
     // TODO: check if message is valid
-    // TODO: dont use unwrap
-    let first_char: char = message.chars().next().unwrap();
-    match first_char {
-        '0'..='9' => {
-            let message_parts: Vec<&str> = match message.split_once(":") {
-                Some((first, second)) => vec![first, second],
-                None => panic!("This is impossible, util is now!"),
-            };        
-            let (decoded_message, _) = decode_string(message_parts[0].parse::<i32>().unwrap(), &message_parts[1]);
-            return decoded_message.to_string();
-    
+    let mut decoded_message: Vec<String> = vec![];
+    let mut rest_of_message: String = "irrelevant".to_string();
+    while rest_of_message != "" {
+        // TODO: dont use unwrap
+        let first_char: char = message.chars().next().unwrap();
+        match first_char {
+            'i' => match message.split_once("e") {
+                Some((main_part, rest_part)) => {
+                    let length: usize = main_part.chars().count() as usize;
+                    decoded_message.push((main_part[1..=length - 1]).to_string());
+                    rest_of_message = rest_part.to_string();
+                }
+                None => {
+                    rest_of_message = "".to_string();
+                }
+            },
+            'l' => {
+                let length: usize = message.chars().count() as usize;
+                let mut _inner_message: &str = &message[1..=length - 2];
+                let temp_decoded_message = decode(_inner_message.to_string());
+                decoded_message.push(format!("\"[{}]\"", temp_decoded_message));
+                rest_of_message = "".to_string();
+            }
+            _ => {
+                match message.split_once(":") {
+                    Some((number_part, text_part)) => {
+                        let char_len = number_part.parse::<i32>().unwrap() as usize;
+                        if char_len <= text_part.len() {
+                            decoded_message
+                                .push(format!("\"{}\"", &text_part[0..char_len]).to_string());
+                            rest_of_message = (&text_part[char_len..]).to_string();
+                        } else {
+                            panic!("Invalid message 1");
+                        };
+                    }
+                    None => {
+                        rest_of_message = "".to_string();
+                    }
+                };
+            }
         }
-        'i' => {
-            let (decoded_message, _) = decode_integer(message);
-            return decoded_message.to_string();
-    
-        }
-        'l' => {
-            let (decoded_message, _) = decode_list(message);
-            return decoded_message.to_string();
-    
-        }
-        _ => return "".to_string()
+        message = rest_of_message.clone();
     }
+    let final_message = decoded_message.join(",");
+    return final_message.to_string();
 }
 
 fn main() {
@@ -55,11 +57,58 @@ fn main() {
 
     match action {
         "decode" => {
-            let decoded_message = decode(message);
+            let decoded_message = decode(message.to_string());
             println!("{}", decoded_message);
         }
         // TODO encode
         _ => panic!("Invalid action"),
     }
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bencoded_string() {
+        let test_cases = vec![
+            ("5:hello", r#""hello""#),
+            ("5:hello13432143124", r#""hello""#),
+            ("15:123456789012345", r#""123456789012345""#),
+            // ("15:12345", ""), TODO: handle this case
+        ];
+
+        for (input, expected) in test_cases {
+            assert_eq!(decode(input.to_string()), expected.to_string());
+        }
+    }
+
+    #[test]
+    fn test_bencoded_int() {
+        let test_cases = vec![
+            ("i52e", "52"),
+            ("i-52e", "-52"),
+            ("i-123456789012345e", "-123456789012345"),
+            ("i52esadw", "52"),
+        ];
+
+        for (input, expected) in test_cases {
+            assert_eq!(decode(input.to_string()), expected.to_string());
+        }
+    }
+
+    #[test]
+    fn test_bencoded_list() {
+        let test_cases = vec![
+            ("l5:helloe", r#""["hello"]""#),
+            ("l5:helloi52ee", r#""["hello",52]""#),
+            ("l5:helloi52ee12345", r#""["hello",52]""#),
+            ("l5:helloi52e5:helloe", r#""["hello",52,"hello"]""#),
+            // ("l5:hellol9:innerlistei52e5:helloe", r#""["hello",52,["innerlist"],52,"hello"]""#), TODO: handle this case
+        ];
+
+        for (input, expected) in test_cases {
+            assert_eq!(decode(input.to_string()), expected.to_string());
+        }
+    }
 }
